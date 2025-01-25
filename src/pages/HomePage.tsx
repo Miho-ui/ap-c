c9d2ba130d0faa1ef802c5a1eb35df76f5713adb
Miho-ui <miho312005@gmail.com>
2025-01-25 16:41:37 +0900

    a

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { supabase } from "../supabase";
 import { useNavigate } from "react-router-dom";
 import { Container, Typography, Button, Box } from "@mui/material";
